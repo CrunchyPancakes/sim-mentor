ab4bb3605d162f52dec6795af60653520873583e
CrunchyPancakes <30matthewt@gmail.com>
2020-01-31 16:12:27 +1300

    Testimonials put into carousel rather than separate.

--- Revised Website/js/script.js
+++ Revised Website/js/script.js
@@ -1,1 +1,65 @@
-//heyo
+// Testimonial Carousel
+var slideIndex = 1;
+
+var slideTimer;
+
+var testimonialContainer;
+
+window.addEventListener("load", function() {
+  showSlides(slideIndex);
+  slideTimer = setInterval(function() {
+    plusSlides(1);
+  }, 4000);
+
+  testimonialContainer = document.getElementsByClassName(
+    "slideshow-container"
+  )[0];
+
+  slideshowContainer.addEventListener("mouseenter", pause);
+  slideshowContainer.addEventListener("mouseleave", resume);
+});
+
+//Controls the current slide and resets interval if needed
+function currentSlide(n) {
+  clearInterval(myTimer);
+  myTimer = setInterval(function() {
+    plusSlides(n + 1);
+  }, 4000);
+  showSlides((slideIndex = n));
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("testimonial-slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+pause = () => {
+  clearInterval(myTimer);
+};
+
+resume = () => {
+  clearInterval(myTimer);
+  myTimer = setInterval(function() {
+    plusSlides(slideIndex);
+  }, 4000);
+};
