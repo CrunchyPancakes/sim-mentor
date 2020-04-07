ea7cb9899d382da356598a37e7e52837b5d0caa8
CrunchyPancakes <30matthewt@gmail.com>
2020-04-07 17:15:38 +1200

    Additional CSS for lead in questions. Courses class in JS to store course information

--- Course Planner/js/script.js
+++ Course Planner/js/script.js
@@ -1,3 +1,17 @@
+// Classes
+class Course {
+  constructor(name) {
+    this.name = name;
+  }
+
+  printName() {
+    console.log(this.name);
+  }
+}
+
+let myCourse = new Course("INFO101");
+window.onload = myCourse.printName();
+
 // Lead in questions
 const questions = document.querySelectorAll(".questions-container");
 console.log(questions[0]);
@@ -6,6 +20,17 @@ let questionIndex = 0;
 const backButton = document.getElementById("back-q");
 const nextButton = document.getElementById("next-q");
 
+// Keeping track of user input
+const majorMinorElectiveElements = document.getElementsByName(
+  "major-minor-elective"
+);
+let majorMinorElectiveInput = "";
+
+const specialisationElements = document.getElementsByName("specialisation");
+let specialisationInput = "";
+
+// Functions
+// Navigating questions
 function backQuestion() {
   if (questionIndex == 0) {
     console.log("Can't go back");
@@ -34,7 +59,38 @@ function displayQuestions() {
   }
 }
 
+// Storing user input
+// Major/Minor/Elective question
+function getRadioInput(radioElements) {
+  let toReturn = null;
+  radioElements.forEach((element) => {
+    if (element.checked) {
+      toReturn = element.id;
+    }
+  });
+
+  return toReturn;
+}
+
+function setMajorMinorElectiveInput() {
+  majorMinorElectiveInput = getRadioInput(majorMinorElectiveElements);
+  console.log(majorMinorElectiveInput);
+}
+
+function setSpecialisationInput() {
+  specialisationInput = getRadioInput(specialisationElements);
+  console.log(specialisationInput);
+}
+
 // Event listeners
 window.onload = displayQuestions();
 backButton.addEventListener("click", backQuestion);
 nextButton.addEventListener("click", nextQuestion);
+
+majorMinorElectiveElements.forEach((element) => {
+  element.addEventListener("click", setMajorMinorElectiveInput);
+});
+
+specialisationElements.forEach((element) => {
+  element.addEventListener("click", setSpecialisationInput);
+});
